8097cf1f5667e7980c160b84d4bbb053f89208b2
Peter Braun <pbraun@redhat.com>
2025-04-27 11:30:21 +0200

    wip: turns

--- ecs/command_turn.go
+++ ecs/command_turn.go
@@ -1,9 +1,7 @@
 package ecs
 
 import (
-	"log"
 	"math"
-	api2 "zuzweit/api"
 )
 
 type Turn struct {
@@ -13,32 +11,12 @@ type Turn struct {
 }
 
 func (a *Turn) Run(dt float64) {
-	translate := a.player.GetComponent(TranslateComponentType).(*TranslateComponent)
+	_ = a.player.GetComponent(TranslateComponentType).(*TranslateComponent)
 	step := a.increment * dt / 1000
-
-	switch a.direction {
-	case api2.North:
-		translate.Y += step
-	case api2.South:
-		translate.Y -= step
-	case api2.East:
-		translate.X -= step
-	case api2.West:
-		translate.X += step
-	}
-
 	a.distance += math.Abs(step)
 }
 
 func (a *Turn) Complete() bool {
-	if a.distance >= 1 {
-		translate := a.player.GetComponent(TranslateComponentType).(*TranslateComponent)
-		translate.X = math.Floor(translate.X) + .5
-		translate.Y = math.Floor(translate.Y) + .5
-		log.Println(translate)
-		return true
-	}
-
 	return false
 }
 
